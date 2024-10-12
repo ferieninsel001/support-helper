18f585803dfdc6a2f2a29962835989df97611cc1
ferieninsel001 <fatales.waivers0e@icloud.com>
2024-10-12 23:03:58 +0200

    update interface

--- interface.py
+++ interface.py
@@ -6,6 +6,7 @@ def clear_console():
 
 def display_start_screen():
     clear_console()
+    print("welcome to the support helper by ferieninsel001")
     print("""
   ____  _   _ ____  ____   ___  ____ _____           _   _ _____ _     ____  _____ ____  
  / ___|| | | |  _ \|  _ \ / _ \|  _ \_   _|         | | | | ____| |   |  _ \| ____|  _ \ 
@@ -13,16 +14,18 @@ def display_start_screen():
   ___) | |_| |  __/|  __/| |_| |  _ < | |   |_____| |  _  | |___| |___|  __/| |___|  _ < 
  |____/ \___/|_|   |_|    \___/|_| \_\|_|           |_| |_|_____|_____|_|   |_____|_| \_\ 
                                                                                          """)
-    print("version 0.0.1 by ferieninsel001")
+    print("https://github.com/ferieninsel001/support-helper")
+    print("version: 0.0.1")
+    print("Licensed under the MIT license")
     time.sleep(1)
 
 def main_menu():
     while True:
-        print("\n")
-        print("1. Sort Desktop ")
-        print("2. Sort Custom Location")
-        print("3. Clear Cache and History")
-        choice = input("opt (1-3): ")
+        print("\nAvailable Commands:")
+        print("1. Sort Desktop\t\t\t Sort all Files on desktop into files folder according to file type")
+        print("2. Sort Custom Location\t\t Sort all Files on custom location into files folder according to file type")
+        print("3. Clear Cache and History\t Clear Cache and History of the most used browsers")
+        choice = input("command (1-3): ")
 
         if choice == '1':
             import tools.file_organizer.desktop_sorter
@@ -31,7 +34,7 @@ def main_menu():
         elif choice == '3':
             import tools.browser_cleaner.cleaner
         elif choice == '':
-            print("Exiting...")
+            print("exiting...")
             time.sleep(1)
             break
         else:
